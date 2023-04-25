26b3dade67cf2f725191967cf7ba30605555b857
Bartlomiej Olszak-Mundzia <bartlomiej.olszak-mundzia@knowit.eu>
2023-04-25 12:37:04 +0200

    Add related_name to Post model

--- TreningDjango/news/models.py
+++ TreningDjango/news/models.py
@@ -10,7 +10,9 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=20)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #Ustawienie related_name powoduje, że User będzie posiadać listę postów pod nazwą 'posts', a nie standardowo 'post_set'
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     category = models.CharField(choices=CATEGORIES, max_length=30)
     created = models.DateTimeField(auto_now_add=True)
 
